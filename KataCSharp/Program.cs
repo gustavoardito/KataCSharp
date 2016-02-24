28b645acba0fd2b7ecf860afda181995d61d6751
Gustavo Ardito <gustavo.ardito@globallogic.com>
2016-02-24 11:19:40 -0300

    create project base

---
+++ KataCSharp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KataCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
