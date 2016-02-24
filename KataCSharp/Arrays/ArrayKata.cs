02f41c9dffb6d6625c330323f626ad4b4dc06d47
Gustavo Ardito <gustavo.ardito@globallogic.com>
2016-02-24 18:19:24 -0300

    implemento kata "program to Rotate array to the right given a pivot"

--- KataCSharp/Arrays/ArrayKata.cs
+++ KataCSharp/Arrays/ArrayKata.cs
@@ -9,7 +9,65 @@ namespace KataCSharp.Arrays
     public class ArrayKata
     {
         public int[] RotateByPivot(int[] items, int pivotIndex) {
-            return items;
+
+            var itemsRotated = items;
+
+            //if (items.Length == 0)
+            //{
+            //    throw new Exception("Empty");
+            //}
+            //else 
+            if (pivotIndex < 0 || pivotIndex > items.Length)
+            {
+                throw new IndexOutOfRangeException();
+            }
+
+            var len = items.Length;
+
+            for (int i = 0; i < len; i++)
+            {
+                //itemsRotated[i] = items[(pivotIndex + i) % len];
+                // rotate right
+                //var pivot = (pivotIndex - i) % len;
+                // rotate left
+                var pivot = (pivotIndex - i + len) % len;
+                itemsRotated[i] = items[pivot];
+            }
+
+            return itemsRotated;
         }
+
+        ////Rotate array to the right of a given pivot
+        //public int[] Rotate(int[] x, int pivot)
+        //{
+        //    if (pivot < 0 || x == null)
+        //        throw new Exception("Invalid argument");
+
+        //    pivot = pivot % x.Length;
+
+        //    //Rotate first half
+        //    x = RotateSub(x, 0, pivot - 1);
+
+        //    //Rotate second half
+        //    x = RotateSub(x, pivot, x.Length - 1);
+
+        //    //Rotate all
+        //    x = RotateSub(x, 0, x.Length - 1);
+
+        //    return x;
+        //}
+
+        //private int[] RotateSub(int[] x, int start, int end)
+        //{
+        //    while (start < end)
+        //    {
+        //        int temp = x[start];
+        //        x[start] = x[end];
+        //        x[end] = temp;
+        //        start++;
+        //        end--;
+        //    }
+        //    return x;
+        //}
     }
 }
--- KataTest/ArrayKataTest.cs
+++ KataTest/ArrayKataTest.cs
@@ -1,4 +1,5 @@
-﻿using KataCSharp.Arrays;
+﻿using FluentAssertions;
+using KataCSharp.Arrays;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -17,18 +18,66 @@ namespace KataTest
         }
 
         [Fact]
-        public void RotateByPivotTest() {
-            var items = new int[] { };
+        public void RotateByPivotNotNullTest() {
+            var items = new int[] { 1 };
             var itemsSorted = srv.RotateByPivot(items, 0);
             Assert.NotNull(itemsSorted);
         }
 
         [Fact]
-        public void RotateByPivotLenghtTest()
+        public void RotateByPivotLengthTest()
         {
-            var items = new int[] { };
+            var items = new int[] { 1 };
             var itemsSorted = srv.RotateByPivot(items, 0);
             Assert.Equal(items.Length, itemsSorted.Length);
         }
+
+        [Fact]
+        public void RotateByPivotIndexExceptionTest()
+        {
+            var items = new int[] { 1 };
+            var itemsSorted = srv.RotateByPivot(items, 0);
+            Action act = () => srv.RotateByPivot(items, -1);
+            var ex = Assert.Throws<IndexOutOfRangeException>(act);
+            Assert.NotNull(ex);
+        }
+
+        [Fact]
+        public void RotateByPivotIndexEquivalentTest()
+        {
+            var items = new int[] {1,3,5,7 };
+            var itemsResult = new int[] { 7, 5, 3, 1 };
+            var itemsRotated = srv.RotateByPivot(items, 0);
+
+            itemsResult.Should().BeEquivalentTo(itemsRotated);
+        }
+
+        [Fact]
+        public void RotateByPivotSortedTest()
+        {
+            var items = new int[] { 1, 3, 5, 7 };
+            var itemsResult = new int[] { 7, 5, 3, 1 };
+            var itemsRotated = srv.RotateByPivot(items, 0);
+
+            itemsResult.Should().ContainInOrder(itemsRotated);
+        }
+        //[Fact]
+        //public void RotateByPivotIndexTest()
+        //{
+        //    var items = new int[] { 1, 3, 5, 7 };
+        //    var itemsResult = new int[] { 7, 5, 3, 1 };
+        //    var itemsRotated = srv.Rotate(items,3);
+
+        //    itemsResult.Should().BeEquivalentTo(itemsRotated);
+        //    //Assert.ar(ex);
+        //}
+
+        //[Fact]
+        //public void RotateByPivotLenghtTest()
+        //{
+        //    var items = new int[] { };
+        //    var itemsSorted = srv.RotateByPivot(items, 0);
+        //    Assert.Equal(items.Length, itemsSorted.Length);
+        //}
     }
 }
