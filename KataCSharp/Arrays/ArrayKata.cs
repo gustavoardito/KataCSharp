2596c11f61af2fdaae9ec4172c8da31af48d0519
Gustavo Ardito <gustavo.ardito@globallogic.com>
2016-02-24 14:05:18 -0300

    create test RotateByPivotTest

---
+++ KataCSharp/Arrays/ArrayKata.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KataCSharp.Arrays
+{
+    public class ArrayKata
+    {
+        public int[] RotateByPivot(int[] items, int pivotIndex) {
+            return null;
+        }
+    }
+}
---
+++ KataTest/ArrayKataTest.cs
@@ -0,0 +1,26 @@
+﻿using KataCSharp.Arrays;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace KataTest
+{
+    public class ArrayKataTest
+    {
+        private readonly ArrayKata srv;
+
+        public ArrayKataTest() {
+            srv = new ArrayKata();
+        }
+
+        [Fact]
+        public void RotateByPivotTest() {
+            var items = new int[] { };
+            var itemsSorted = srv.RotateByPivot(items, 0);
+            Assert.NotNull(itemsSorted);
+        }
+    }
+}
